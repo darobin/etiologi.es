b9ecc320883b756318636cd467a83b8fd70c98d2
Robin Berjon <robin@berjon.com>
2015-05-27 15:03:56 -0400

    generating about page correctly

--- tools-src/tasks/process-html.js
+++ tools-src/tasks/process-html.js
@@ -9,11 +9,53 @@ function processHTML (source, target) {
     return new Promise((resolve, reject) => {
         fs.readFile(source, "utf8", (err, data) => {
             if (err) return reject(err);
-            let $ = whacko.load(data);
-            // XXX
+            let $ = whacko.load(data)
+            ,   $title = $("title")
+            ,   $body = $("body")
+            ;
             // change head
+            $("meta[charset]").after('<meta name="viewport" content="width=device-width">');
+            $title.after("<link rel='stylesheet' href='/css/etiologies.css'>");
+            
+            // main
+            let $main = $("<main></main>").append($("body").contents());
+            $body.append($main);
+            $main.prepend($("<h1></h1>").text($title.text()));
+            
             // header
+            $body.prepend(
+                `<header>
+                  <iframe src="/img/tree.svg" width="960" height="288"></iframe>
+                  <p class="site-title">Etiologies</p>
+                  <nav>
+                    <ul>
+                      <li><a href="/">home</a></li>
+                      <li><a href="/notes/">notes</a></li>
+                      <li><a href="/books/">books</a></li>
+                      <li><a href="/about/">about</a></li>
+                    </ul>
+                  </nav>
+                </header>`
+            );
+
             // footer
+            $body.append(
+                `<footer>
+                  <p class="signature">
+                    <img src="/img/bug.svg" width="80" height="80" alt="Robin Berjon Logo">
+                    Robin Berjon
+                  </p>
+                  <p class="self-links">
+                    <a href="mailto:robin@berjon.com">robin@berjon.com</a> ・
+                    <a href="http://berjon.com/">http://berjon.com/</a> ・
+                    <a href="https://twitter.com/robinberjon">@robinberjon</a>
+                  </p>
+                  <p class="license">
+                    <a href="http://creativecommons.org/licenses/by/4.0/" rel="license">CC-BY</a>
+                  </p>
+                </footer>`
+            );
+            
             fs.outputFile(target, $.html(), (err) => {
                 if (err) return reject(err);
                 resolve();
--- tools/tasks/process-html.js
+++ tools/tasks/process-html.js
@@ -9,11 +9,24 @@ function processHTML(source, target) {
     return new Promise(function (resolve, reject) {
         fs.readFile(source, "utf8", function (err, data) {
             if (err) return reject(err);
-            var $ = whacko.load(data);
-            // XXX
+            var $ = whacko.load(data),
+                $title = $("title"),
+                $body = $("body");
             // change head
+            $("meta[charset]").after("<meta name=\"viewport\" content=\"width=device-width\">");
+            $title.after("<link rel='stylesheet' href='/css/etiologies.css'>");
+
+            // main
+            var $main = $("<main></main>").append($("body").contents());
+            $body.append($main);
+            $main.prepend($("<h1></h1>").text($title.text()));
+
             // header
+            $body.prepend("<header>\n                  <iframe src=\"/img/tree.svg\" width=\"960\" height=\"288\"></iframe>\n                  <p class=\"site-title\">Etiologies</p>\n                  <nav>\n                    <ul>\n                      <li><a href=\"/\">home</a></li>\n                      <li><a href=\"/notes/\">notes</a></li>\n                      <li><a href=\"/books/\">books</a></li>\n                      <li><a href=\"/about/\">about</a></li>\n                    </ul>\n                  </nav>\n                </header>");
+
             // footer
+            $body.append("<footer>\n                  <p class=\"signature\">\n                    <img src=\"/img/bug.svg\" width=\"80\" height=\"80\" alt=\"Robin Berjon Logo\">\n                    Robin Berjon\n                  </p>\n                  <p class=\"self-links\">\n                    <a href=\"mailto:robin@berjon.com\">robin@berjon.com</a> ・\n                    <a href=\"http://berjon.com/\">http://berjon.com/</a> ・\n                    <a href=\"https://twitter.com/robinberjon\">@robinberjon</a>\n                  </p>\n                  <p class=\"license\">\n                    <a href=\"http://creativecommons.org/licenses/by/4.0/\" rel=\"license\">CC-BY</a>\n                  </p>\n                </footer>");
+
             fs.outputFile(target, $.html(), function (err) {
                 if (err) return reject(err);
                 resolve();
